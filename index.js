ba8d23a8286ee914cc826e1e7d535091a85ddefe
ziadweb <39879421+ziadweb@users.noreply.github.com>
2022-03-10 21:46:38 +0200

    Dice-Game

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImages= "dice"+ randomNumber1 + ".png";
+var randomImageSource= "images/"+randomDiceImages;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImages= "dice"+randomNumber2+".png";
+var randomImageSource2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Player 1 Wins👌";
+}
+else if (randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Player 2 Wins👌";
+}
+else
+document.querySelector("h1").innerHTML="IT IS DRAW 😒";
